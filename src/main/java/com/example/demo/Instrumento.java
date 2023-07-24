ae28e61e824cf62b12adbb0cfdb5e167cddda5eb
lauraOlortegui <140428110+lauraOlortegui@users.noreply.github.com>
2023-07-24 19:42:49 +0000

    ec3 agregar el render

---
+++ src/main/java/com/example/demo/Instrumento.java
@@ -0,0 +1,35 @@
+package com.example.demo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "instrumento")
+public class Instrumento {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
+
+    private String nombre;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    
+}
